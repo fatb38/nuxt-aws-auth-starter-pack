0433abdf78b6ff9738462658da13dc51e0a53b7d
fatb38 <arno.tgh@gmail.com>
2020-11-12 23:13:29 +0100

    adding an Auth plugin with few global methods to fetch user information

--- nuxt.config.js
+++ nuxt.config.js
@@ -34,6 +34,8 @@ export default {
 
   // Plugins to run before rendering page (https://go.nuxtjs.dev/config-plugins)
   plugins: [
+    '~/plugins/amplify.js',
+    '~/plugins/auth.js'
   ],
 
   // Auto import components (https://go.nuxtjs.dev/config-components)
