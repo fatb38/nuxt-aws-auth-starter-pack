2d09df6ed50703e036cc75e60691c709eeccd4ff
fatb38 <arno.tgh@gmail.com>
2020-11-13 00:33:11 +0100

    adding index page

--- pages/index.vue
+++ pages/index.vue
@@ -1,9 +1,34 @@
 <template>
-  <div>Hello World</div>
+  <v-container>
+    <div class="d-flex flex-column justify-center mt-4">
+      <v-alert
+        outlined
+        text
+        color="success"
+        border="left"
+        dense
+      >
+        Welcome to the home page <span class="primary--text">{{ $auth.email }}</span><br>
+        You're logged in !
+      </v-alert>
+      <v-btn
+        small
+        block
+        color="primary lighten-1"
+        @click="$store.dispatch('auth/logout')"
+      >
+        Logout
+      </v-btn>
+    </div>
+  </v-container>
 </template>
 
 <script>
-export default {
-
-}
+export default {}
 </script>
+
+<style scoped>
+div {
+  width: 300px;
+}
+</style>
